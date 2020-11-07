bf9fc6ec9b83c40f89ec49d78beee33ca4849ede
touzov1012 <alex@touzov.com>
2020-11-06 21:37:16 -0500

    cleaned references

--- PolyNesterDemo/ObjHandler.cs
+++ PolyNesterDemo/ObjHandler.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace PolyNesterDemo
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
 
     public class UVObjData
     {
